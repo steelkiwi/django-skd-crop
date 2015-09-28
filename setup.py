dd12c8abbf05a01459310ed0d2d11278b1064c04
Andriy Hordiychuk <hordiychuk@steelkiwi.com>
2015-09-28 16:23:15 +0300

    Required files and application's skeleton

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+VERSION = open(os.path.join(here, 'VERSION')).read()
+
+requires = [
+    'Pillow',
+    'six',
+]
+
+setup(name='django-skd-crop',
+      version=VERSION,
+      description='Easy working with images and crop',
+      long_description=README,
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.4",
+          "Framework :: Django",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+      ],
+      author='SteelKiwi',
+      url='https://github.com/steelkiwi/django-skd-crop',
+      keywords='web django crop image field',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires)
